7f9d1c87976f85a57f1a8778dbbd7fad31ae5f3a
Kehayov98 <alexandar793@gmail.com>
2022-05-20 14:32:32 +0300

    Add

--- store/store/settings.py
+++ store/store/settings.py
@@ -49,6 +49,7 @@ STORE_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + STORE_APPS
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,12 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
